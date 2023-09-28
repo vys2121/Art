c0da0afd1f53df2d4a9d9931380a25e21559af98
Vyom Singh <135890092+vys2121@users.noreply.github.com>
2023-09-28 23:42:26 +0530

    Update settings.py

--- social_media/settings.py
+++ social_media/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-6(13n&q1@1f5(f2ez6$-nzdznt)22kxe*p=)#9-e%jhmnz001l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
 
-ALLOWED_HOSTS = ['artsocial.azurewebsites.net']
+ALLOWED_HOSTS = ['arts1.azurewebsites.net']
 
 
 # Application definition
