317284e6d25031c247031bfaac38a4bae7f1fa09
Vyom Singh <135890092+vys2121@users.noreply.github.com>
2023-09-29 10:31:12 +0530

    Update urls.py

--- social_media/urls.py
+++ social_media/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path('', include("main.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
